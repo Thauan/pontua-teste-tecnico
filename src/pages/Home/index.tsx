6c92d0ac7e66e6728d3af4b80f8271604fa8bdd2
Thauan Almeida <thaubr28@gmail.com>
2023-08-06 10:36:44 -0300

    feat: marvel api service call

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from './styles';
+import { getCharacters } from '../../services/characters';
 
 const Home: React.FC = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [characters, setCharacters] = useState<any>([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(characters);
+
+  const getData = async () => {
+    const data = await getCharacters();
+    setCharacters(data);
+  }
   return (
     <Container>
       <h1>HOME</h1>
